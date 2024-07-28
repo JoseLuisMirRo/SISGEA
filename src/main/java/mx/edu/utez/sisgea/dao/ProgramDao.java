58deed926307e3695b443624c4accc3bca97c8c6
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-07-28 17:50:01 -0600

    Refactor on programDao to optimize connections

--- src/main/java/mx/edu/utez/sisgea/dao/ProgramDao.java
+++ src/main/java/mx/edu/utez/sisgea/dao/ProgramDao.java
@@ -3,6 +3,7 @@ package mx.edu.utez.sisgea.dao;
 import mx.edu.utez.sisgea.model.ProgramBean;
 import mx.edu.utez.sisgea.utility.DataBaseConnection;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,21 +11,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProgramDao extends DataBaseConnection {
+    private Connection con = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
     public List<ProgramBean> getAllPrograms() {
         List<ProgramBean> programs = new ArrayList<ProgramBean>();
         try {
-            PreparedStatement ps = createConnection().prepareStatement("SELECT * FROM program");
-            ResultSet rs = ps.executeQuery();
+            con = createConnection();
+            ps = con.prepareStatement("SELECT * FROM program");
+            rs = ps.executeQuery();
 
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 programs.add(new ProgramBean(id, name));
             }
-            ps.close();
-            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            closeConnection();
         }
         return programs;
     }
@@ -32,18 +38,31 @@ public class ProgramDao extends DataBaseConnection {
     public ProgramBean getProgram(int id) {
         ProgramBean program = null;
         try{
-            PreparedStatement ps = createConnection().prepareStatement("SELECT * FROM program WHERE id = ?");
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
+            con = createConnection();
 
-            while(rs.next()) {
+            ps = con.prepareStatement("SELECT * FROM program WHERE id = ?");
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if(rs.next()) {
                 int idE = rs.getInt("id");
                 String name = rs.getString("name");
                 program = new ProgramBean(id, name);
             }
         }catch (SQLException e){
             e.printStackTrace();
+        }finally {
+            closeConnection();
         }
         return program;
+    }
+    private void closeConnection() {
+        try {
+            if (ps != null) ps.close();
+            if (rs != null) rs.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
