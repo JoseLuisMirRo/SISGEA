abb530ffe556799adfdf4e4f32679464bdcffcca
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-07-28 17:46:41 -0600

    Refactor on LoginDao to optimize DB Connections

--- src/main/java/mx/edu/utez/sisgea/dao/LoginDao.java
+++ src/main/java/mx/edu/utez/sisgea/dao/LoginDao.java
@@ -4,28 +4,31 @@ import mx.edu.utez.sisgea.model.LoginBean;
 import mx.edu.utez.sisgea.model.RoleBean;
 import mx.edu.utez.sisgea.utility.DataBaseConnection;
 
-import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class LoginDao extends DataBaseConnection {
+    private Connection con = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
     public LoginBean loginValidate(LoginBean login) throws SQLException {
         String email = login.getEmail();
         String password = login.getPassword();
-        CallableStatement st = null;
-        ResultSet rs = null;
 
         try{
             boolean status = false;
             int id;
             List<RoleBean> roles = new ArrayList<RoleBean>();
-            st = createConnection().prepareCall("SELECT id, email, firstname,lastnamep,lastnamem,status FROM user WHERE email = ? AND password =?");
-            st.setString(1, email);
-            st.setString(2,password);
-            st.execute();
-            rs = st.getResultSet();
+            con = createConnection();
+            ps = con.prepareStatement("SELECT id, email, firstname,lastnamep,lastnamem,status FROM user WHERE email = ? AND password =?");
+            ps.setString(1, email);
+            ps.setString(2,password);
+            rs = ps.executeQuery();
 
             if(rs.next()) {
                 id=rs.getInt("id");
@@ -35,8 +38,6 @@ public class LoginDao extends DataBaseConnection {
                 login.setLastNameP(rs.getString("lastnamep"));
                 login.setLastNameM(rs.getString("lastnamem"));
                 status = rs.getBoolean("status");
-                st.close();
-                rs.close();
                 if(status) {
                     if (email.equals(login.getEmail()) && password.equals(login.getPassword())) {
                         return login;
@@ -49,8 +50,19 @@ public class LoginDao extends DataBaseConnection {
             }
         }catch (Exception e){
             throw new SQLException(e.getMessage());
+        }finally {
+            closeDBConnection();
         }
         return null;
+    }
+    private void closeDBConnection() {
+        try {
+            if (ps != null) ps.close();
+            if (rs != null) rs.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void closeConnection() throws SQLException{
