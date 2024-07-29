a5d7af771b93a3b5660594cab728e1364cf8d95f
Jose Luis Miranda Roldán <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-07-29 12:59:56 -0600

    Refactor QuarterDao to optimize SQL Connections

--- src/main/java/mx/edu/utez/sisgea/dao/QuarterDao.java
+++ src/main/java/mx/edu/utez/sisgea/dao/QuarterDao.java
@@ -3,34 +3,38 @@ package mx.edu.utez.sisgea.dao;
 import mx.edu.utez.sisgea.model.QuarterBean;
 import mx.edu.utez.sisgea.utility.DataBaseConnection;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuarterDao extends DataBaseConnection {
+    private Connection con = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
     public void insertQuarter(QuarterBean quarter) throws SQLException {
         try{
-            PreparedStatement ps = createConnection().prepareStatement("insert into quarter(name,startdate,enddate) values(?,?,?)");
+            con = createConnection();
+            ps = con.prepareStatement("INSERT INTO quarter(name,startdate,enddate) VALUES (?,?,?)");
             ps.setString(1, quarter.getName());
             ps.setDate(2, quarter.getStartDate());
             ps.setDate(3, quarter.getEndDate());
             ps.execute();
-            ps.close();
         }catch (SQLException e){
             e.printStackTrace();
             throw new SQLException(e.getMessage());
+        }finally {
+            closeConnection();
         }
     }
 
     public QuarterBean getQuarter(int id) {
         QuarterBean quarter = null;
         try{
-            PreparedStatement ps = createConnection().prepareStatement("select * from quarter where id=?");
+            con=createConnection();
+            ps = con.prepareStatement("SELECT * FROM quarter WHERE id=?");
             ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
 
             if (rs.next()) {
                 int idE = rs.getInt("id");
@@ -41,6 +45,8 @@ public class QuarterDao extends DataBaseConnection {
             }
         }catch (SQLException e){
             e.printStackTrace();
+        }finally {
+            closeConnection();
         }
         return quarter;
     }
@@ -48,23 +54,33 @@ public class QuarterDao extends DataBaseConnection {
     public List<QuarterBean> getAllQuarters() {
         List<QuarterBean> quarters = new ArrayList<QuarterBean>();
         try {
-            PreparedStatement ps = createConnection().prepareStatement("select * from quarter");
-            ResultSet rs = ps.executeQuery();
+            con=createConnection();
+            ps = con.prepareStatement("select * from quarter");
+            rs = ps.executeQuery();
 
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 Date startDate = rs.getDate("startdate");
                 Date endDate = rs.getDate("enddate");
-                quarters.add(new QuarterBean(id,name,startDate,endDate));
+                quarters.add(new QuarterBean(id, name, startDate, endDate));
             }
-            ps.close();
-            rs.close();
             return quarters;
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection();
         }
         return List.of();
+    }
+    private void closeConnection() {
+        try {
+            if (ps != null) ps.close();
+            if (rs != null) rs.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
