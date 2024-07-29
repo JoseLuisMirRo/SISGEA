084db0864aeeeb23de340602f9a72c3fa08a3255
Jose Luis Miranda Roldán <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-07-29 14:26:25 -0600

    Refactor UserDao to optimize SQL Connections

--- src/main/java/mx/edu/utez/sisgea/dao/UserDao.java
+++ src/main/java/mx/edu/utez/sisgea/dao/UserDao.java
@@ -4,38 +4,41 @@ import mx.edu.utez.sisgea.model.UserBean;
 import mx.edu.utez.sisgea.model.UserroleBean;
 import mx.edu.utez.sisgea.utility.DataBaseConnection;
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao extends DataBaseConnection {
+    private Connection con = null;
+    private CallableStatement cs = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
     private RoleDao roleDao = new RoleDao();
 
     public int insertUser(UserBean user) throws SQLException {
         try{
             int userId=0;
             boolean result=false;
-            CallableStatement cs = createConnection().prepareCall("INSERT INTO user (email,firstname,lastnamep,lastnamem,password,status) VALUES (?, ?, ?, ?, ?, ?)");
-            cs.setString(1, user.getEmail());
-            cs.setString(2, user.getFirstName());
-            cs.setString(3, user.getLastNameP());
-            cs.setString(4, user.getLastNameM());
-            cs.setString(5, user.getPassword());
-            cs.setBoolean(6, user.isStatus());
-            result = cs.execute();
+            con = createConnection();
+            ps = con.prepareStatement("INSERT INTO user (email,firstname,lastnamep,lastnamem,password,status) VALUES (?, ?, ?, ?, ?, ?)");
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getFirstName());
+            ps.setString(3, user.getLastNameP());
+            ps.setString(4, user.getLastNameM());
+            ps.setString(5, user.getPassword());
+            ps.setBoolean(6, user.isStatus());
+            result = ps.execute();
 
-            ResultSet rs = cs.getGeneratedKeys();
+            rs = ps.getGeneratedKeys();
             while (rs.next()) {
                 userId = rs.getInt(1);
             }
-            cs.close();
             if(!result) return userId;
         } catch (Exception e){
             e.printStackTrace();
             throw new SQLException(e.getMessage());
+        } finally {
+            closeConnection();
         }
         return 0;
     }
@@ -43,12 +46,12 @@ public class UserDao extends DataBaseConnection {
     public UserBean getUser(int id) throws SQLException {
         UserBean user = null;
         try {
-            PreparedStatement st = createConnection().prepareCall("SELECT * FROM user WHERE id=?");
-            st.setInt(1, id);
-            st.execute();
-            ResultSet rs = st.getResultSet();
+            con = createConnection();
+            ps = con.prepareStatement("SELECT * FROM user WHERE id=?");
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
 
-            while (rs.next()) {
+            if (rs.next()) {
                 int idE = rs.getInt("id");
                 String email = rs.getString("email");
                 String firstname = rs.getString("firstname");
@@ -57,10 +60,10 @@ public class UserDao extends DataBaseConnection {
                 boolean status = rs.getBoolean("status");
                 user = new UserBean(idE, email, firstname, lastNameP, lastNameM, status);
             }
-            st.close();
-            rs.close();
         }catch (Exception e){
             throw new SQLException(e.getMessage());
+        }finally {
+            closeConnection();
         }
         return user;
     }
@@ -68,9 +71,10 @@ public class UserDao extends DataBaseConnection {
     public List<UserBean> readAllUsers() {
         try {
             List<UserBean> usersList = new ArrayList<UserBean>();
-            CallableStatement cs = createConnection().prepareCall("SELECT * FROM user");
-            cs.execute();
-            ResultSet rs = cs.getResultSet();
+            con = createConnection();
+            ps = con.prepareStatement("SELECT * FROM user");
+            rs = ps.executeQuery();
+
             while(rs.next()) {
                 int id = rs.getInt("id");
                 String email = rs.getString("email");
@@ -80,61 +84,74 @@ public class UserDao extends DataBaseConnection {
                 boolean status = rs.getBoolean("status");
                 usersList.add(new UserBean(id,email,firstname,lastNameP,lastNameM,status));
             }
-            cs.close();
-            rs.close();
             return usersList;
-
         }catch (Exception e) {
             e.printStackTrace();
-
+        }finally {
+            closeConnection();
         }
         return List.of();
     }
 
     public void updateUser(UserBean user) throws SQLException {
         try{
-            CallableStatement cs = createConnection().prepareCall("UPDATE user SET email=?,firstname=?,lastnamep=?,lastnamem=?,password=? WHERE id=?");
-            cs.setString(1, user.getEmail());
-            cs.setString(2,user.getFirstName());
-            cs.setString(3,user.getLastNameP());
-            cs.setString(4,user.getLastNameM());
-            cs.setString(5, user.getPassword());
-            cs.setInt(6,user.getId());
-            cs.execute();
-            cs.close();
-
+            con = createConnection();
+            ps = con.prepareStatement("UPDATE user SET email=?,firstname=?,lastnamep=?,lastnamem=?,password=? WHERE id=?");
+            ps.setString(1, user.getEmail());
+            ps.setString(2,user.getFirstName());
+            ps.setString(3,user.getLastNameP());
+            ps.setString(4,user.getLastNameM());
+            ps.setString(5, user.getPassword());
+            ps.setInt(6,user.getId());
+            ps.execute();
         }catch (Exception e){
             e.printStackTrace();
             throw new SQLException(e.getMessage());
+        }finally {
+            closeConnection();
         }
     }
 
     public int deleteUser(int id) throws SQLException {
         try{
-            CallableStatement cs = createConnection().prepareCall("UPDATE user SET status=? WHERE id=?");
-            cs.setBoolean(1,false);
-            cs.setInt(2,id);
-            cs.execute();
-            cs.close();
+            con = createConnection();
+            ps = con.prepareStatement("UPDATE user SET status=? WHERE id=?");
+            ps.setBoolean(1,false);
+            ps.setInt(2,id);
+            ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
             throw new SQLException(e.getMessage());
+        }finally {
+            closeConnection();
         }
         return 0;
     }
 
     public int revertDeleteUser(int id) throws SQLException {
         try{
-            CallableStatement cs = createConnection().prepareCall("UPDATE user SET status=? WHERE id=?");
-            cs.setBoolean(1,true);
-            cs.setInt(2,id);
-            cs.execute();
-            cs.close();
+            con = createConnection();
+            ps = con.prepareStatement("UPDATE user SET status=? WHERE id=?");
+            ps.setBoolean(1,true);
+            ps.setInt(2,id);
+            ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
             throw new SQLException(e.getMessage());
+        }finally {
+            closeConnection();
         }
         return 0;
+    }
+
+    private void closeConnection() {
+        try {
+            if (ps != null) ps.close();
+            if (rs != null) rs.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
 
